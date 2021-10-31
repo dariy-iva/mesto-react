bf9e357699579abcfec56b1a0333a1b29ba1391f
Daria Ivanova <dashann@mail.ru>
2021-10-31 09:42:58 +0300

    Updates

--- src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-		<footer className="footer">
-			<p className="footer__author">&copy;&nbsp;2021 Daria Ivanova</p>
-		</footer>
-  );
-}
